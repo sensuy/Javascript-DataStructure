b4b3b7683c3c2619b48a0a46f8e538e86468d8dc
sensui <sensuijunior@hotmail.com>
2023-06-29 21:14:10 -0300

    linear time

---
+++ data-structure/find-nemo.js
@@ -0,0 +1,14 @@
+const memo = ["nemo"];
+const evryone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
+
+const large = new Array(100000).fill("nemo");
+
+function findMemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Found Nemo!");
+    }
+  }
+}
+
+findMemo(large); // O(n) --> Linear Time
