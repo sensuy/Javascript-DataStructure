ed0860a8159a1822b50c2a59f9f25b9832c5393b
Hailton Junior <hailton.junior@ilia.digital>
2023-07-04 18:27:44 -0300

    hash tables

--- coding-problems/3-reverse-string.js
+++ coding-problems/3-reverse-string.js
@@ -1,50 +1,14 @@
-// function reverseString(str) {
-//   // your code here
-//   // "Hi My name is Andrei"
-//   const arrayOfLetters = str.split("");
+function reverseString(str) {
+  // your code here
+  // "Hi My name is Andrei"
+  const arrayOfLetters = str.split("");
 
-//   for (let i = 0; i < arrayOfLetters.length / 2; i++) {
-//     const temp = arrayOfLetters[i];
-//     arrayOfLetters[i] = arrayOfLetters[arrayOfLetters.length - 1 - i];
-//     arrayOfLetters[arrayOfLetters.length - 1 - i] = temp;
-//   }
-//   return arrayOfLetters.join("");
-// }
-
-// console.log(reverseString("Hi My name is Andrei"));
-
-function mergeSortedArrays(array1, array2) {
-  const mergedArray = [];
-  let array1Item = array1[0];
-  let array2Item = array2[0];
-
-  let i = 1;
-  let j = 1;
-
-  // Check input
-  if (array1.length === 0) {
-    return array2;
+  for (let i = 0; i < arrayOfLetters.length / 2; i++) {
+    const temp = arrayOfLetters[i];
+    arrayOfLetters[i] = arrayOfLetters[arrayOfLetters.length - 1 - i];
+    arrayOfLetters[arrayOfLetters.length - 1 - i] = temp;
   }
-
-  if (array2.length === 0) {
-    return array1;
-  }
-
-  while (array1Item || array2Item) {
-    console.log(array1Item, array2Item);
-    if (!array2Item || array1Item < array2Item) {
-      mergedArray.push(array1Item);
-      array1Item = array1[i];
-      i++;
-    } else {
-      mergedArray.push(array2Item);
-      array2Item = array2[j];
-      j++;
-    }
-  }
-
-  return mergedArray;
-  // return mergedArray.concat(array1.slice(i)).concat(array2.slice(j));
+  return arrayOfLetters.join("");
 }
 
-console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+console.log(reverseString("Hi My name is Andrei"));
