e61cfe15e529b12f1884616c4312632bc33a7b9c
Hailton Junior <hailton.junior@ilia.digital>
2023-07-26 07:42:40 -0300

    queues implementations

--- data-structure/7-queues.js
+++ data-structure/7-queues.js
@@ -12,3 +12,48 @@ class Node {
   }
 }
 
+// Linked List implementation
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // Return the first element
+  peek() {
+    return this.first;
+  }
+
+  // Add to the end
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.size === 0) {
+      this.first = newNode;
+    } else {
+      this.last.next = newNode;
+    }
+
+    this.last = newNode;
+
+    this.size++;
+    return this;
+  }
+
+  // Remove from the beginning
+  dequeue() {
+    if (this.size === 0) return null;
+
+    const temp = this.first;
+
+    if (this.size === 1) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size--;
+
+    return temp.value;
+  }
+}
